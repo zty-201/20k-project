5ba13094b52dbf6dbcd14f946c91ac7b255f9add
Anson <anson.t@digipen.edu>
2025-03-12 15:47:36 +0800

    Test Test

--- topic_modelling.py
+++ topic_modelling.py
@@ -8,3 +8,4 @@ def extract_topics(text_data, num_topics=5):
     lda_model = LatentDirichletAllocation(n_components=num_topics, random_state=42)
     lda_model.fit(transformed_data)
     return lda_model, vectorizer
+#hello
